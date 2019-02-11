784f58bf7927dfd15fa4892f796c6f1c7b9f89dc
Sergey Volkov <s.volkov@netris.ru>
2019-02-11 17:25:35 +0300

    Add "raw-config" and modules injection support

--- hooks/build.js
+++ hooks/build.js
@@ -11,6 +11,50 @@ var buildConfig;
 var path = require('path');
 var fs = require('fs');
 
+function isArray(obj) {
+  return Object.prototype.toString.call(obj) === '[object Array]';
+}
+
+function extendArray(to, from, prefix) {
+  from.forEach(function(item, idx) {
+    if (to.indexOf(item) !== -1) {
+      return;
+    }
+    var copy;
+    if (typeof item === 'object') {
+      copy = {};
+      extendObject(copy, item, prefix + '[' + idx + ']')
+    } else {
+      copy = item;
+    }
+    to.push(copy);
+  })
+}
+
+function extendObject(to, from, prefix)  {
+  Object.keys(from).forEach(function(key) {
+    var newPrefix = prefix + '.' + key;
+    var type = typeof to[key];
+    if (isArray(from[key])) {
+      if (type === 'undefined') {
+        to[key] = [];
+      } else if (!isArray(to[key])) {
+        throw TypeError('Wrong type "' + type + '" of ' + newPrefix);
+      }
+      return extendArray(to[key], from[key], newPrefix);
+    }
+    if (typeof from[key] === 'object') {
+      if (type === 'undefined') {
+        to[key] = {};
+      } else if (type !== 'object') {
+        throw TypeError('Wrong type "' + type + '" of ' + newPrefix);
+      }
+      return extendObject(to[key], from[key], newPrefix);
+    }
+    to[key] = from[key];
+  })
+}
+
 /**
  * @param {Object} logger Logger.
  * @param {Object} config Configuration object.
@@ -75,42 +119,58 @@ exports.init = function(logger, config, cli, appc) {
    * Injecting profile parameters to tiapp.
    */
   cli.addHook('build.pre.construct', function(build, finished) {
-    if (!!profile.properties) {
-      Object.keys(profile.properties).forEach(function(key) {
-        var prop = build.tiapp.properties[key];
-        var value = profile.properties[key];
-        if (!prop) {
-          var type;
-          if (typeof value === 'boolean') type = 'bool';
-          else if (typeof value === 'number') type = 'number';
-          else type = 'string';
-          prop = {type: type};
-          build.tiapp.properties[key] = prop;
-        }
-        prop.value = profile.properties[key];
-      });
-    }
 
-    build.tiapp.properties['ti.multiconfiguration.profile'] = {
-      type: 'string',
-      value: p
-    };
+    if (profile['raw-config']) {
+      extendObject(build, profile['raw-config'], 'config');
+      if (profile['raw-config']['tiapp'] && profile['raw-config']['tiapp']['modules']) {
+        build.validate(logger, config, cli)(function() {
 
-    build.tiapp.id = profile.id;
-    build.tiapp.name = profile.name;
-    if (typeof buildConfig.VERSION !== 'undefined') {
-      build.tiapp.version = buildConfig.VERSION;
-    }
-
-    if (typeof profile['android-theme'] !== 'undefined') {
-      var themeId = profile['android-theme'];
-      if (build.tiappAndroidManifest) {
-        if (!build.tiappAndroidManifest.application) {
-          build.tiappAndroidManifest.application = {};
-        }
-        build.tiappAndroidManifest.application.theme = '@style/Theme.Multiconfiguration.' + themeId;
+          //Inject again, because it was rewritten on validation
+          extendObject(build, profile['raw-config'], 'config');
+          sync();
+        });
+      } else {
+        sync();
       }
     }
-    finished();
+    function sync() {
+      if (!!profile.properties) {
+        Object.keys(profile.properties).forEach(function(key) {
+          var prop = build.tiapp.properties[key];
+          var value = profile.properties[key];
+          if (!prop) {
+            var type;
+            if (typeof value === 'boolean') type = 'bool';
+            else if (typeof value === 'number') type = 'number';
+            else type = 'string';
+            prop = {type: type};
+            build.tiapp.properties[key] = prop;
+          }
+          prop.value = profile.properties[key];
+        });
+      }
+
+      build.tiapp.properties['ti.multiconfiguration.profile'] = {
+        type: 'string',
+        value: p
+      };
+
+      build.tiapp.id = profile.id;
+      build.tiapp.name = profile.name;
+      if (typeof buildConfig.VERSION !== 'undefined') {
+        build.tiapp.version = buildConfig.VERSION;
+      }
+
+      if (typeof profile['android-theme'] !== 'undefined') {
+        var themeId = profile['android-theme'];
+        if (build.tiappAndroidManifest) {
+          if (!build.tiappAndroidManifest.application) {
+            build.tiappAndroidManifest.application = {};
+          }
+          build.tiappAndroidManifest.application.theme = '@style/Theme.Multiconfiguration.' + themeId;
+        }
+      }
+      finished();
+    }
   });
 };
